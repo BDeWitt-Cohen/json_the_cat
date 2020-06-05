5f02fa6d14fa0b307c261d6ef9db7f9c2180ee4e
Trevor Thomas <thomastrevor1@gmail.com>
2020-06-05 14:23:11 +0000

    all tests working, linted

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,27 @@
 const request = require('request');
 
-// https://api.thecatapi.com/v1/breeds/search?q=sib to search
+const fetchBreedDescription = (breedName, cb) => {
 
-// guery example ?q=sib
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      cb(error);
+    }
 
-let args = process.argv.slice(2);
+    const data = JSON.parse(body);
 
-let query = args[0];
+    if (data.length === 0) {
+      cb('Breed Not Found');
+    } else {
+      cb(null, data[0].description.trim());
+    }
+  });
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${query}`, (error, response, body) => {
-  if (error) {
-    throw new Error(`There was an issue with your cat search, and your error is.... : ${error}`);
-  }
-  const data = JSON.parse(body);
+};
 
-  if (data.length === 0) {
-    console.log('Sorry, that breed wasn\'t found!');
-  }
 
-  console.log(data[0].description);
+module.exports = { fetchBreedDescription };
 
-});
+
+
+
+
